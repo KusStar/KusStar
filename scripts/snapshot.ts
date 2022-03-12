950a94154814cb1ee6dacf2e9eb59538b371b2f0
Kuss <kussssss@outlook.com>
2022-03-13 01:25:20 +0800

    :sparkles: feat: root:snapshot with KussRenderer

--- scripts/snapshot.ts
+++ scripts/snapshot.ts
@@ -1,4 +1,62 @@
-import { renderToGif } from '@kuss/headless'
+import { BasicRenderer, Config } from '@kuss/headless'
 import kusstar from 'kusstar'
+import * as THREE from 'three'
 
-renderToGif(kusstar as any, { quality: 10 })
+class KussRenderer extends BasicRenderer {
+  kussMesh: THREE.Mesh
+  km2: THREE.Mesh
+  km22: THREE.Mesh
+  km3: THREE.Mesh
+  km33: THREE.Mesh
+
+  constructor(config: Partial<Config>) {
+    super(config)
+
+    this.camera.position.z = 3
+    this.#addMeshes()
+  }
+
+  #addMeshes() {
+    this.kussMesh = this.getMeshFromVox(kusstar)
+
+    this.km2 = this.kussMesh.clone()
+    this.km2.scale.multiplyScalar(0.3)
+
+    this.km22 = this.km2.clone()
+    this.km3 = this.km2.clone()
+    this.km33 = this.km2.clone()
+
+    this.scene.add(this.kussMesh, this.km2, this.km22, this.km3, this.km33)
+  }
+
+  raf() {
+    super.raf()
+
+    const angle = Math.PI / this.totalFrame * 2
+    const factor = 2
+
+    this.kussMesh.rotation.y += angle
+
+    this.km2.rotation.y += angle
+    this.km22.rotation.y += angle
+
+    this.km3.rotation.y += angle
+    this.km33.rotation.y += angle
+
+    this.km2.position.x = Math.sin(this.kussMesh.rotation.y * factor)
+    this.km2.position.z = Math.cos(this.kussMesh.rotation.y * factor)
+
+    this.km22.position.x = Math.sin(this.kussMesh.rotation.y * factor + Math.PI / 3) * 1.3
+    this.km22.position.z = Math.cos(this.kussMesh.rotation.y * factor + Math.PI / 3) * 1.3
+
+    this.km3.position.x = Math.sin(this.kussMesh.rotation.y * factor + Math.PI / 2)
+    this.km3.position.y = Math.cos(this.kussMesh.rotation.y * factor + Math.PI / 2)
+
+    this.km33.position.x = Math.sin(this.kussMesh.rotation.y * factor) * 1.3
+    this.km33.position.y = Math.cos(this.kussMesh.rotation.y * factor) * 1.3
+  }
+}
+
+const kussRenderer = new KussRenderer({ quality: 10 })
+
+kussRenderer.start()
